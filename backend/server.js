f4c30bc3b9b08d7ac595dbcbf8a07c3e115e3a6a
rikity8 <145489381+rikity8@users.noreply.github.com>
2025-05-21 19:16:05 +0300

    Server update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
-const loginRoute = require('./routes/login'); 
+const loginRoute = require('./routes/login');
+const registerRoute = require('./routes/register'); 
 
 const app = express();
 const port = 3000;
@@ -28,6 +29,7 @@ app.use(bodyParser.json());
 
 // Подключаем маршруты для входа и регистрации
 app.use('/api', loginRoute);  // Запросы через /api/login и /api/register
+app.use('/api/register', registerRoute);
 
 // Старт сервера
 app.listen(port, () => {
